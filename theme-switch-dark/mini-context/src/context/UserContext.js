a8e422fd53729ff0daf8e5fd8a4f7c363565b967
Ashish <ashish.uniyal599@gmail.com>
2024-04-01 01:52:44 +0530

    finalizing repo

--- theme-switch-dark/mini-context/src/context/UserContext.js
+++
@@ -1,7 +0,0 @@
-// This context is for user data for login, hence it is called UserContext.
-
-import React from "react";
-
-const UserContext = React.createContext();
-
-export default UserContext;
